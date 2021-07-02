90d1b3dccc3d272bf20b730dbf16502f8cf8a30f
Ilnur Timergaliev <49416213+mhufflep@users.noreply.github.com>
2021-07-02 22:04:33 +0500

    Create bd_lstdup.c

---
+++ src/bd_lstdup.c
@@ -0,0 +1,26 @@
+static void *
+bd_memmove(void *dst, const void *src, size_t n)
+{
+    if (dst == NULL && src == NULL)
+        return (0);
+    while (n-- > 0)
+        ((unsigned char *)dst)[n] = ((unsigned char *)src)[n];
+    return (dst);
+}
+
+t_blst *
+bd_lstdup(t_blst *lst, int datasize)
+{
+    t_blst  *new;
+    void    *new_data;
+
+    new_data = malloc(datasize);
+    if (!new_data)
+        return (NULL);
+    if (lst->data)
+        bd_memmove(new_data, lst->data, datasize);
+    new = bd_lstnew(new_data);
+    if (!new)
+        free(new_data);
+    return (new);
+}
